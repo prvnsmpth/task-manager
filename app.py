f1142a0cca5b7c156e0e6dd1477eaa9bca5c1302
Praveen Sampath <psampath1@linkedin.com>
2017-01-22 10:57:53 +0530

    Test slash command

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 import web
+import json
         
 urls = (
-    '/hello', 'hello',
     '/events', 'EventHandler'
 )
 app = web.application(urls, globals())
 wsgiapp = app.wsgifunc()
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'World'
-        return 'Hello, ' + name + '!'
-
 class EventHandler:
 
+    def handle_install(self, event):
+        pass
+    
+    def handle_command(self, event):
+        return event['text']
+
     def POST(self):
-        data = web.input()
-        print 'User ID', data
-        return 'OK'
+        event = json.loads(web.data())
+
+        event_name = event['name']
+
+        if event_name == 'app.install':
+            self.handle_install(event)
+            return 'OK'
+        elif event_name == 'client.slashCommand':
+            response = self.handle_command(event)
+            return 'You said: {0}'.format(response)
 
 if __name__ == "__main__":
     app.run()
